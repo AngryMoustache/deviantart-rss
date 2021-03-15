2d7c37c2caf90436d2058b2af1f796f710d1c8e7
AngryMoustache <sander.vandamme11@gmail.com>
2021-03-15 17:27:17 +0100

    Initial commit

---
+++ src/DeviantartRssServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AngryMoustache\DeviantartRss;
+
+use Illuminate\Support\ServiceProvider;
+
+class DeviantartRssServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
